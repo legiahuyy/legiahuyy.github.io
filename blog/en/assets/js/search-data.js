a01b62b6ac5ccd6c0c79affa497bc374832445e4
legiahuyy <69957076+legiahuyy@users.noreply.github.com>
2021-07-04 18:21:52 +0700

    New theme

---
+++ blog/en/assets/js/search-data.js
@@ -0,0 +1,36 @@
+var store = [{
+        "title": "A README.md",
+        "excerpt":"Welcome to my personal blog where I can share to people my thoughts and experience about (mostly) technology, research articles and sometimes irrelevant stuff. Topics on this blog are mainly about cyber research, school projects, club tasks or CTF write-ups. I will try to keep the blog updated regularly, one...","categories": ["Thoughts"],
+        "tags": ["thoughts"],
+        "url": "https://legiahuyy.github.io/blog/en/thoughts/README/"
+      },{
+        "title": "KCSC - LFI/RFI Wrapper",
+        "excerpt":"This is the first iteration of my CTF write-up series for the new year. Today we will be solving one of my university’s initial challenges. The task is simple itself but I want to show you the mindset of how I play the game. Fingerprinting Manual testing The absolute first...","categories": [],
+        "tags": ["kma","web-pentest"],
+        "url": "https://legiahuyy.github.io/blog/en/RFI-Wrapper/"
+      },{
+        "title": "KCSC - A simple BOF",
+        "excerpt":"Today we are going to test out Ghidra with one of my university pwnie challenge. Info Value Name pwn1 SHA256 d3e77076e7c9c113e22d08ff1e266aad4070625fb9841f49451dc835b09bd799d243e24a064ba3e8b0f10d86f5d10b381942f6902343d614ee5cf401c4138fd3 Type ELF 32-bit LSB executable, Intel 80386, version 1 (SYSV), dynamically linked, interpreter /lib/ld-linux.so.2, for GNU/Linux 3.2.0 Entropy 3.82492 (not packed) Analyzing Let’s load the binary into our disassembler...","categories": [],
+        "tags": ["kma","reverse-engineer","pwn"],
+        "url": "https://legiahuyy.github.io/blog/en/ez-bof/"
+      },{
+        "title": "HackTheBox - Sharp",
+        "excerpt":"Hi, after a long time not posting anything on this blog because of my university workload. Let’s get back to our normal routine of pwning. Today, I will do a writeup of retired HackTheBox (HTB) machine - Sharp, which is rated 4.8 pts. For anyone who doesn’t know about HTB,...","categories": ["hackthebox"],
+        "tags": ["hackthebox","network","pwn"],
+        "url": "https://legiahuyy.github.io/blog/en/hackthebox/HTB-Sharp/"
+      },{
+        "title": "HackTheBox - Armageddon",
+        "excerpt":"This is another challenge on HackTheBox - Armageddon. Let’s get started. Enumeration Nmap We start by enumerating open ports and services on the target machine using nmap: ┌──(root💀kali)-[/home/kali/HackTheBox/Armageddon]└─# cat nmap/armageddon.nmap # Nmap 7.91 scan initiated Thu May 27 00:25:58 2021 as: nmap -sS -sC -sV -p- -oA nmap/armageddon -v 10.10.10.233Nmap...","categories": ["hackthebox"],
+        "tags": ["hackthebox","network","pwn"],
+        "url": "https://legiahuyy.github.io/blog/en/hackthebox/HTB-Armageddon/"
+      },{
+        "title": "HackTheBox - The Notebook",
+        "excerpt":"Welcome back to our usual HackTheBox journey, for today medium rated target - The Notebook. In this blog post, we’ve managed to alter client-side JWT cookie, spawn our reverse shell then proceeded to achieve user’s SSH credentials and, subsequently, be able to privesc via a docker PoC. Enumeration The very...","categories": ["hackthebox"],
+        "tags": ["hackthebox","network","pwn"],
+        "url": "https://legiahuyy.github.io/blog/en/hackthebox/HTB-TheNotebook/"
+      },{
+        "title": "Update.txt",
+        "excerpt":"Greetings everyone, it has been a few weeks since our last posts. You may (or may not) have already noticed, I changed the theme of our blog since this one looks more professional and less boring than the old Chirpy theme I had used. But the new theme switch is...","categories": ["Thoughts"],
+        "tags": ["content","miscs"],
+        "url": "https://legiahuyy.github.io/blog/en/thoughts/Update-Note/"
+      }]
