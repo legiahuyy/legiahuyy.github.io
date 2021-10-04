d7bdd5c26204f77076f8cf6855cf084727360604
legiahuyy <69957076+legiahuyy@users.noreply.github.com>
2021-10-04 11:47:36 +0700

    renew

---
+++ blog/en/assets/js/data/cache-list.js
@@ -0,0 +1,490 @@
+const include = [
+
+  /*--- CSS ---*/
+
+  '/blog/en/assets/css/home.css',
+  '/blog/en/assets/css/categories.css',
+  '/blog/en/assets/css/tags.css',
+  '/blog/en/assets/css/archives.css',
+  '/blog/en/assets/css/page.css',
+  '/blog/en/assets/css/post.css',
+  '/blog/en/assets/css/category-tag.css',
+  '/blog/en/assets/css/lib/bootstrap-toc.min.css',
+
+  /*--- Javascripts ---*/
+
+  '/blog/en/assets/js/home.min.js',
+  '/blog/en/assets/js/page.min.js',
+  '/blog/en/assets/js/post.min.js',
+
+  /*--- HTML ---*/
+
+  /* Tabs */
+  
+
+  /* The posts of first Home page and recent update list */
+  
+
+  
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+
+  
+
+  
+    '/blog/en/posts/HTB-BountyHunter/',
+  
+    '/blog/en/posts/HTB-Cap/',
+  
+    '/blog/en/posts/PRE-C1-E1/',
+  
+    '/blog/en/posts/Update-Note/',
+  
+    '/blog/en/posts/HTB-TheNotebook/',
+  
+    '/blog/en/posts/HTB-Armageddon/',
+  
+    '/blog/en/posts/HTB-Sharp/',
+  
+    '/blog/en/posts/0/',
+  
+    '/blog/en/posts/1/',
+  
+    '/blog/en/posts/2/',
+  
+    '/blog/en/posts/3/',
+  
+
+  /* Trending tags */
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+      
+        
+        
+
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+      
+        
+        
+
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+      
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+      
+        
+        
+
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+      
+        
+        
+
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+      
+    
+  
+    
+    
+    
+    
+      
+        
+        
+
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+      
+    
+  
+    
+    
+    
+    
+      
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+      
+        
+        
+
+  
+    
+    
+    
+    
+      
+        
+        
+
+  
+    
+    
+    
+    
+      
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+      
+        
+        
+
+  
+    
+    
+    
+    
+      
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+      
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+      
+        
+        
+
+  
+    
+    
+    
+    
+      
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+      
+    
+  
+    
+    
+    
+    
+  
+    
+    
+    
+    
+      
+    
+  
+    
+    
+    
+    
+      
+        
+        
+
+
+  
+    
+    '/blog/en/tags/pwn/',
+  
+    
+    '/blog/en/tags/hackthebox/',
+  
+    
+    '/blog/en/tags/network/',
+  
+    
+    '/blog/en/tags/kma/',
+  
+    
+    '/blog/en/tags/machine/',
+  
+    
+    '/blog/en/tags/reverse+engineer/',
+  
+    
+    '/blog/en/tags/content/',
+  
+    
+    '/blog/en/tags/miscs/',
+  
+    
+    '/blog/en/tags/on-going/',
+  
+    
+    '/blog/en/tags/practical+reverse+engineering/',
+  
+
+  /*--- Icons ---*/'/blog/en/assets/img/favicons/favicon.ico',
+  '/blog/en/assets/img/favicons/apple-icon.png',
+  '/blog/en/assets/img/favicons/apple-icon-precomposed.png',
+  '/blog/en/assets/img/favicons/apple-icon-57x57.png',
+  '/blog/en/assets/img/favicons/apple-icon-60x60.png',
+  '/blog/en/assets/img/favicons/apple-icon-72x72.png',
+  '/blog/en/assets/img/favicons/apple-icon-76x76.png',
+  '/blog/en/assets/img/favicons/apple-icon-114x114.png',
+  '/blog/en/assets/img/favicons/apple-icon-120x120.png',
+  '/blog/en/assets/img/favicons/apple-icon-144x144.png',
+  '/blog/en/assets/img/favicons/apple-icon-152x152.png',
+  '/blog/en/assets/img/favicons/apple-icon-180x180.png',
+  '/blog/en/assets/img/favicons/android-icon-192x192.png',
+  '/blog/en/assets/img/favicons/favicon-32x32.png',
+  '/blog/en/assets/img/favicons/favicon-96x96.png',
+  '/blog/en/assets/img/favicons/favicon-16x16.png',
+  '/blog/en/assets/img/favicons/ms-icon-144x144.png',
+  '/blog/en/assets/img/favicons/manifest.json',
+  '/blog/en/assets/img/favicons/browserconfig.xml',
+
+  /*--- Others ---*/
+
+  '/blog/en/assets/js/data/search.json',
+  '/blog/en/404.html',
+
+  '/blog/en/app.js',
+  '/blog/en/sw.js'
+];
+
+const exclude = ['/assets/js/data/pageviews.json',
+  '/img.shields.io/'
+];
+
---
+++ blog/en/assets/js/data/swcache.js
@@ -0,0 +1,60 @@
+const resource = [
+
+  /* --- CSS --- */
+  '/blog/en/assets/css/style.css',
+
+  /* --- JavaScripts --- */
+  
+  '/blog/en/assets/js/dist/home.min.js',
+  '/blog/en/assets/js/dist/page.min.js',
+  '/blog/en/assets/js/dist/post.min.js',
+  '/blog/en/assets/js/dist/categories.min.js',
+  '/blog/en/assets/js/data/search.json',
+  '/blog/en/app.js',
+  '/blog/en/sw.js',
+
+  /* --- HTML --- */
+  '/blog/en/index.html',
+  '/blog/en/404.html',
+  
+    '/blog/en/about/',
+  
+    '/blog/en/archives/',
+  
+    '/blog/en/categories/',
+  
+    '/blog/en/tags/',
+  
+
+  /* --- Favicons --- */
+  
+
+  '/blog/en/assets/img/favicons/android-chrome-192x192.png',
+  '/blog/en/assets/img/favicons/android-chrome-512x512.png',
+  '/blog/en/assets/img/favicons/apple-touch-icon.png',
+  '/blog/en/assets/img/favicons/favicon-16x16.png',
+  '/blog/en/assets/img/favicons/favicon-32x32.png',
+  '/blog/en/assets/img/favicons/favicon.ico',
+  '/blog/en/assets/img/favicons/mstile-150x150.png',
+  '/blog/en/assets/img/favicons/site.webmanifest',
+  '/blog/en/assets/img/favicons/browserconfig.xml'
+
+];
+
+/* The request url with below domain will be cached */
+const allowedDomains = [
+  
+
+  'legiahuyy.github.io',
+
+  'fonts.gstatic.com',
+  'fonts.googleapis.com',
+  'cdn.jsdelivr.net',
+  'polyfill.io'
+];
+
+/* Requests that include the following path will be banned */
+const denyUrls = [
+  
+];
+
