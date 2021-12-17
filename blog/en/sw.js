820906a9cacec30389ce105ad7aeb605cb103736
legiahuyy <69957076+legiahuyy@users.noreply.github.com>
2021-12-17 08:02:51 +0700

    grammar fix

--- blog/en/sw.js
+++ blog/en/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/en/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20211216.1744';
+const cacheName = 'chirpy-20211217.0902';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
