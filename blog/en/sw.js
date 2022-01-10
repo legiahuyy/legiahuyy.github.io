b0ef9edba738b78babb3750146bf6ac153c2732f
legiahuyy <69957076+legiahuyy@users.noreply.github.com>
2022-01-10 11:51:49 +0700

    update

--- blog/en/sw.js
+++ blog/en/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/en/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220110.1250';
+const cacheName = 'chirpy-20220110.1251';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
